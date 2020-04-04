8d66f779c417284ee1a27ce8b328973b930d574d
Imre Tabur <imre.tabur@eesti.ee>
2020-04-04 17:08:35 +0300

    version-1.1.7

--- release.sh
+++ release.sh
@@ -4,15 +4,23 @@
 # Copyright (c) 2017-2019 Imre Tabur <imre.tabur@eesti.ee>
 
 NAME=js-api-extend
-RELEASE=1.1.6
+RELEASE=1.1.7
 VERSION_TAG=version-${RELEASE}
 
-npm ci && npm run build && npm run test && npm run unit
-git add ./dist package.json package-lock.json release.sh src/main/webapp/js/${NAME}.min.js
+npm install
+npm audit fix
+npm ci
+npm run build
+npm run test
+npm run unit
+git add ./dist package.json package-lock.json release.sh ./src/main/webapp/js/${NAME}.min.js
 git commit -m "${VERSION_TAG}"
 git push
 git checkout master
 git merge develop
-git tag -a ${VERSION_TAG} -m "${VERSION_TAG}" && git push origin ${VERSION_TAG} && git push && npm publish
+git tag -a ${VERSION_TAG} -m "${VERSION_TAG}"
+git push origin ${VERSION_TAG}
+git push
+npm publish
 
 exit 0
